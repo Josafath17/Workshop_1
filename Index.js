1e73917c636b6a8331c8a5d4851f5d1201113750
Josafath17 <105898971+Josafath17@users.noreply.github.com>
2024-02-07 23:29:56 -0600

    corrigiendo el error
    No lo subi a git la primera vez que lo hice

---
+++ Index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const routes = require('./routes/routes');
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use('/api', routes)
+const router = express.Router()
+module.exports = router;
+
+
+
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+
